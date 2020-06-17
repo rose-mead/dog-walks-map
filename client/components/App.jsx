767aaabaf516c5cce0f64545d8e4cb67b68cff8a
student <student@eda-bc-03.localdomain>
2020-06-18 11:56:55 +1200

    Set up from boilerplate

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import WalksList from './WalksList'
+import data from '../../server/public/data.json'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <h1>Dog walks in the Waikato</h1>
+        <WalksList data={data} />
+
+        <Route path='/walk' component={Walk}/>
+      </div>
+    </Router>
+
+
   )
 }
 
---
+++ client/components/WalksList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+
+function WalksList(props) {
+  return <div>
+    <ul> {
+        props.data.map(walk => {
+            return <li><Link to='/walk'>{walk.name}</Link></li>
+            // return <a href={walk.name}>{walk.name}</a>
+        })
+    }
+    </ul>
+  </div>
+
+  
+}
+
+export default WalksList
