79c1b90964c97e046edc22ddb9efbf44720ea64b
student <student@eda-bc-07.localdomain>
2020-06-24 16:46:45 +1200

    Add markers

--- client/components/MapDiv.jsx
+++ client/components/MapDiv.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import request from 'superagent'
-import { Map, GoogleApiWrapper } from 'google-maps-react'
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
 
 class MapDiv extends React.Component {
-    state = {}
+    state = {
+        locations: [{lat:-37.980, lng:175.311},{lat:-37.769, lng:175.356}, {lat:-37.677, lng:175.236}]
+    }
+    // -37.980780, 175.311337 yarndleys
+    // -37.769881, 175.356829 newstead
+    // {lat:-37.677, lng:175.236}  kainui
+
+    displayMarkers = () => {
+        return this.state.locations.map((walk, index) => {
+          return <Marker key={index} id={index} position={{
+           lat: walk.lat,
+           lng: walk.lng
+         }}
+         onClick={() => console.log("You clicked me!")} />
+        })
+
+      }
+    
 
     render() {
         return (
@@ -12,7 +29,12 @@ class MapDiv extends React.Component {
                 zoom={9}
                 style={this.mapStyles}
                 initialCenter={{lat:-37.677, lng:175.236}}
-            />
+                >
+                    {/* <Marker position={{lat:-37.980, lng:175.311}} />
+                    <Marker position={{lat:-37.769, lng:175.356}} />
+                    <Marker position={{lat:-37.677, lng:175.236}} /> */}
+                    {this.displayMarkers()}
+                </Map>
         )
     }
 }
