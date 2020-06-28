568229d6c62a1d588eed06c7b890e54883c98b5d
Rose-Mead <rmkmead@hotmail.com>
2020-06-28 12:43:51 +1200

    Expose the api to get the walks from the db

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,10 +4,9 @@ const router = express.Router()
 
 
 router.get('/walks', (req, res) => {
-    console.log("in route function")
     db.getWalks()
     .then(walks => {
-        console.log(walks)
+        res.json(walks)
     })
 })
 
