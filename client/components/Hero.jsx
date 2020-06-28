baa428189ef5aa82136d9e19f32b3a08292f7093
Rose-Mead <rmkmead@hotmail.com>
2020-06-28 14:36:22 +1200

    Add some styling

---
+++ client/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return(
+        <div className="hero">
+            <div className="welcome">
+                <h1>Dog walks in the Waikato</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
