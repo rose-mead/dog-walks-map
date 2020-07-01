504871746cda27426b6337b2ee5218bbc13d91e9
Rose-Mead <rmkmead@hotmail.com>
2020-07-01 15:15:18 +1200

    Styled walk page

--- client/components/Walk.jsx
+++ client/components/Walk.jsx
@@ -11,52 +11,22 @@ class Walk extends React.Component {
     walk: {}
   }
 
-  // componentDidMount() {
-  //   getWalk(this.props.match.params.id)
-  //     .then(walk => {
-  //       this.setState({
-  //         walk: walk
-  //       })
-  //     })
-
-  // }
-
-  findWalk = () => {
-    // return this.props.walks.filter(walk => walk.id == this.props.match.params.id)
-  }
-
-  // id = this.props.match.params.id
-
-  // componentDidMount() {
-  //   getWalks()
-  //     .then(walks => {
-  //       this.setState({ walks: walks })
-
-  //       return walks.find(walk => {
-  //         return walk.id == this.id
-  //       })
-  //     })
-  //     .then(walk => {
-  //       this.setState({ walk: walk })
-  //     })
-  // }
-
-
-  // Could access the current walk from props - but it wasn't loading
-  // console.log(this.props.walks.filter(walk => walk.id == this.props.match.params.id))
 
   render() {
-    return <div className='walk'>
-
-      <ul>
+    return (
+      <div className='walk'>
         <h3>{this.props.walk.name}</h3>
-        <li>{this.props.walk.description}</li>
-        <li>{this.props.walk.location}</li>
-        <li>{this.props.walk.time}</li>
+        <p>{this.props.walk.location}</p>
 
-      </ul>
-      <Link to={'/'}>Home</Link>
-    </div>
+        <div className='walk-details'>
+          <p>Time: {this.props.walk.time}</p>
+          <p>Difficulty: {this.props.walk.difficulty}</p>
+        </div>
+        <p>{this.props.walk.description}</p>
+
+        <Link to={'/'}>Home</Link>
+      </div>
+    )
   }
 }
 
